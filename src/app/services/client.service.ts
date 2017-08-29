a5cdcddd23df6d76c169f905b66002e0696aaf46
Katarina1330 <Katarina@Katarinas-MacBook-Pro.local>
2017-08-28 17:30:21 -0700

    Implementing client details

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -21,4 +21,9 @@ export class ClientService {
     this.clients.push(client);
   }
 
+  getClient(id:string) {
+    this.client = this.af.object('/clients/' + id) as FirebaseObjectObservable<Client>;
+    return this.client;
+  }
+
 }
